2ca0d2cb4c117a21c855c6ef62fc9b420e12282b
Emilia Calvillo <a330703@alumnos.uaslp.mx>
2024-09-18 20:50:42 -0600

    Cuarto commit, home terminado

--- lib/home.dart
+++ lib/home.dart
@@ -9,129 +9,346 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> listaTareas = List.from(con.listaExamen);
+
+  void eliminarCard(int index) {
+    String id = listaTareas[index].split('#')[0];
+
+    if (id == '010') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Se eliminó la card con ID=$id'),
+        ),
+      );
+    } else {
+      setState(() {
+        listaTareas.removeAt(index);
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Se eliminó la card con ID=$id'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: con.background,
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 10.0),
-              const Align(
-                alignment: Alignment.center,
+        child: Column(
+          children: [
+            const SizedBox(height: 10.0),
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'SEGUNDA VISTA: EMILIA NATHALIE CALVILLO RAYÓN',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
                 child: Text(
-                  'SEGUNDA VISTA: EMILIA NATHALIE CALVILLO RAYÓN',
+                  'Lista de tareas pendientes',
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.0),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: con.titles,
+                  ),
                   textAlign: TextAlign.center,
                 ),
               ),
+            ),
+            const SizedBox(height: 25.0),
 
-              const SizedBox(height: 10.0),
+            // ListView.builder
+            Expanded(
+              child: ListView.builder(
+                itemCount: listaTareas.length,
+                itemBuilder: (context, index) {
+                  List<String> data = listaTareas[index].split('#');
+                  String id = data[0];
+                  String numero = data[1];
+                  String titulo = data[2];
+                  String descripcion = data[3];
+                  int estrellas = int.parse(data[4]);
 
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  padding: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
+                  bool isSecondCard = int.parse(numero) % 2 == 0; //Según número (par=botones, impar=sin botones)
+
+                  return Column(
+                    children: [
+                      if (isSecondCard)
+                        CreateSecondCard(
+                          numero: numero,
+                          titulo: titulo,
+                          descripcion: descripcion,
+                          estrellas: estrellas,
+                          onDelete: () => eliminarCard(index),
+                        )
+                      else
+                        CreateFirstCard(
+                          numero: numero,
+                          titulo: titulo,
+                          descripcion: descripcion,
+                          estrellas: estrellas,
+                          onDelete: () => eliminarCard(index),
+                        ),
+                      const SizedBox(height: 10.0),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// FirstCard class
+class CreateFirstCard extends StatelessWidget {
+  final String numero;
+  final String titulo;
+  final String descripcion;
+  final int estrellas;
+  final VoidCallback onDelete;
+
+  const CreateFirstCard({
+    super.key,
+    required this.numero,
+    required this.titulo,
+    required this.descripcion,
+    required this.estrellas,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      height: 150.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0), // Corner radius
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    numero,
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      color: con.background,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(
+                      Icons.task,
+                      size: 30.0,
+                      color: con.background4,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    titulo,
+                    style: TextStyle(fontSize: 22.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    descripcion,
+                    style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: List.generate(estrellas, (index) {
+                        return Icon(Icons.star, size: 20.0, color: Colors.yellow);
+                      }),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// SecondCard class
+class CreateSecondCard extends StatelessWidget {
+  final String numero;
+  final String titulo;
+  final String descripcion;
+  final int estrellas;
+  final VoidCallback onDelete;
+
+  const CreateSecondCard({
+    super.key,
+    required this.numero,
+    required this.titulo,
+    required this.descripcion,
+    required this.estrellas,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      height: 150.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 9,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          numero,
+                          style: TextStyle(
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.bold,
+                            color: con.background,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Icon(
+                            Icons.task,
+                            size: 30.0,
+                            color: con.background4,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          titulo,
+                          style: TextStyle(fontSize: 22.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          descripcion,
+                          style: TextStyle(fontSize: 13.0, color: Colors.grey),
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: List.generate(estrellas, (index) {
+                              return Icon(Icons.star, size: 20.0, color: Colors.yellow);
+                            }),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: con.background2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                   child: Text(
-                    'Lista de tareas pendientes',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: con.titles,
-                    ),
-                    textAlign: TextAlign.center, // Centrar el texto en el contenedor
+                    'Ver más',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 25.0),
-
-              // First Card
-              Container(
-                padding: EdgeInsets.all(15.0),
-                height: 150.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0), // Corner radius
+                SizedBox(height: 10.0),
+                ElevatedButton(
+                  onPressed: onDelete,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Borrar',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                // Container for rows
-                child: Column(
-                  children: [
-                    // First Row
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text('2', style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold, color: con.background)),
-                          ),
-                          Expanded(
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Icon(
-                                  Icons.task,
-                                  size: 30.0,
-                                  color: con.background4,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Second Row
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text('Título tarea',
-                            style: TextStyle(fontSize: 22.0),
-                          ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Third Row
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text('descripción de la actividad',
-                              style: TextStyle(
-                                  fontSize: 18.0,
-                                  color: Colors.grey
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Icon(
-                                Icons.star,
-                                size: 20.0,
-                                color: Colors.yellow,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 25.0),
-
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,17 +10,18 @@ const effects = Color(0xff919b02);
 
 
 const List listaExamen = [
+  //Número de lista 2 elementos de 2 al 13 (12 en total)
   //ID # Número(2-13) # titulo # descripción # num de estrellas en amarillo
-  '1#2#Lavar la ropa blanca#Antes de que llueva#3',
-  '2#3#Llevar la bicicleta a reparar#Al taller de siempre#3',
-  '3#4#Ir a surtir la despensa#Al Soriana que queda de paso#4',
-  '4#5#Recoger el paquete que llegó#En sucursal DHL#4',
-  '5#6#Pagar la factura de la luz#Vence en 3 días#5',
-  '6#7#Pasar a la pastelería#Por el pastel de cumpleaños#5',
-  '7#8#Pasar al cajero#Para sacar efectivo#2',
-  '8#9#Comprar croquetas#Para las bellas mascotas#5',
-  '9#10#Recoger la computadora del mantenimiento#Al lugar nuevo#2',
-  '10#11#Pasar a la farmacia#Para surtir la loratadina#1',
-  '11#12#Comprar flores frescas#Para regalar de decoración#1',
-  '12#13#Recoger el saco de la tintorería#Mañana sin falta#1',
+  '002#2#Lavar la ropa blanca#Antes de que llueva#3',
+  '003#3#Llevar la bicicleta a reparar#Al taller de siempre#3',
+  '004#4#Ir a surtir la despensa#Al Soriana que queda de paso#4',
+  '005#5#Recoger el paquete que llegó#En sucursal DHL#4',
+  '006#6#Pagar la factura de la luz#Vence en 3 días#5',
+  '007#7#Pasar a la pastelería#Por el pastel de cumpleaños#5',
+  '008#8#Pasar al cajero#Para sacar efectivo#2',
+  '009#9#Comprar croquetas#Para las bellas mascotas#5',
+  '010#10#Recoger la computadora#Al lugar nuevo#2',
+  '011#11#Pasar a la farmacia#Para surtir la loratadina#1',
+  '012#12#Comprar flores frescas#Para regalar de decoración#1',
+  '013#13#Recoger el saco de la tintorería#Mañana sin falta#1',
 ];
